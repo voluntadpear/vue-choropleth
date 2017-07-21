ec6f9bdd65c1c4d71cdcb4dcf06704f9a36fb18f
Guillermo Peralta Scura <gperaltascura@gmail.com>
2017-07-21 15:12:59 -0400

    Update example to use JSON file

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <ChoroplethMap :data="pyDepartmentsData" titleKey="department_name" idKey="department_id" :value="value" :extraValues="extraValues" geojsonIdKey="dpto" :geojson="geojson" :center="center" :colorScale="colorScale" referenceTitle="Girls school enrolment" dataTitle="Department" dataPlaceholder="Hover over a department" mapStyle="height: 500px;" :zoom="6" :mapOptions="mapOptions">
+    <ChoroplethMap :data="pyDepartmentsData" titleKey="department_name" idKey="department_id" :value="value" :extraValues="extraValues" geojsonIdKey="dpto" :geojson="paraguayGeojson" :center="center" :colorScale="colorScale" referenceTitle="Girls school enrolment" dataTitle="Department" dataPlaceholder="Hover over a department" mapStyle="height: 500px;" :zoom="6" :mapOptions="mapOptions">
     </ChoroplethMap>
   </div>
 </template>
@@ -8,6 +8,7 @@
 <script>
 import ChoroplethMap from 'vue-choropleth'
 import { geojson } from './data/py-departments-geojson'
+import paraguayGeojson from './data/paraguay.json'
 import { pyDepartmentsData } from './data/py-departments-data'
 
 export default {
@@ -17,7 +18,7 @@ export default {
     return {
       center: [-23.752961, -57.854357],
       pyDepartmentsData,
-      geojson,
+      paraguayGeojson,
       colorScale: ["e7d090", "e9ae7b", "de7062"],
       value: {
         key: "amount_w",
