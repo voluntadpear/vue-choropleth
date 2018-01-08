6ac3db9f54e278816c5ff8bea804170762a02b07
Guillermo Peralta Scura <gperaltascura@gmail.com>
2018-01-08 09:15:10 -0300

    Update build step

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-../node_modules/.bin/webpack --hide-modules --colors --progress --config webpack.js --display-error-details
